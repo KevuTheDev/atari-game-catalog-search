a7e584ac818d5fe53eb8cea0cf21947769dcd4a2
Kevin He <kevin66he@hotmail.com>
2022-07-11 13:54:29 -0400

    Pushing Test PHP

---
+++ test/test/test1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="author" content="Kevin He">
+    <title>Atari Game Catalog | CP 476 Project</title>
+</head>
+
+<body>
+    <a href="test.php">Home</a>
+    <h1>This is a header. :)</h1>
+
+</body>
+
+
+</html>
